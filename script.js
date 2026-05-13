1260a4b55a3edd64d2f3cc75734ce7feb2d14a89
Mahfooz Alam <ma.mahfoozalam8789@gmail.com>
2026-05-13 22:04:03 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerText == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerText == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerText == '⌫'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerText;
+            input.value = string
+        }
+
+    })
+})
